28ad56e084d79c79860e01f09aadf7ca2145f670
Julian Hangstörfer <jhangstoerfer@gmail.com>
2014-10-27 19:14:02 +0100

    InstaSynch changed to MIT License

--- InstaSynchP-Settings.user.js
+++ InstaSynchP-Settings.user.js
@@ -6,7 +6,7 @@
 // @version     1.0.7
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Settings
-// @license     GPL-3.0
+// @license     MIT
 
 // @include     http://*.instasynch.com/*
 // @include     http://instasynch.com/*
